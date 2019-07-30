c8887d2cd3fb8c68a33e58a43ad2a365670401db
Blessedadudu <47356974+Blessedadudu@users.noreply.github.com>
2019-07-30 23:10:14 +0100

    Updates

---
+++ precache-manifest.28affa45aed962f6b34721126f6b837c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends-knowledge-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c965b2e44b784ee34c84",
+    "url": "/robofriends-knowledge-app/static/js/runtime~main.fd3dafe1.js"
+  },
+  {
+    "revision": "da59569894c572a3e30c",
+    "url": "/robofriends-knowledge-app/static/js/main.8880b560.chunk.js"
+  },
+  {
+    "revision": "7014fc0e3f08cf713f04",
+    "url": "/robofriends-knowledge-app/static/js/2.9bb725c1.chunk.js"
+  },
+  {
+    "revision": "da59569894c572a3e30c",
+    "url": "/robofriends-knowledge-app/static/css/main.4b6586c6.chunk.css"
+  },
+  {
+    "revision": "7014fc0e3f08cf713f04",
+    "url": "/robofriends-knowledge-app/static/css/2.9c8059d3.chunk.css"
+  },
+  {
+    "revision": "a9381c68e9715e7536e7bd45ee94bc28",
+    "url": "/robofriends-knowledge-app/index.html"
+  }
+];
